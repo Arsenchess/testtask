cca16e209274c9362590d4e7e7311bc03664f418
Arsen Fedoseev <arsen.fedoseev@mytona.com>
2025-06-01 14:51:10 +0700

    test task

---
+++ test_playwright_title.py
@@ -0,0 +1,7 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+def test_page_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expected_title = "Fast and reliable end-to-end testing for modern web apps | Playwright"
+    expect(page).to_have_title(expected_title)
